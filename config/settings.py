9544741bb43a682ce5c843ca1e8ccf7a853f90ac
Kris <ssspers92@gmail.com>
2024-06-07 17:22:10 +0300

    Created fields for User and reformatted code

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,4 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 PHONENUMBER_DEFAULT_REGION = "RU"
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -11,3 +11,12 @@ class User(AbstractUser):
     avatar = models.ImageField(
         upload_to="users/", blank=True, null=True, verbose_name="Аватар"
     )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
