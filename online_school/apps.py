9dc4b5774fa47da99c977273ff718133bcbd3e40
Kris <ssspers92@gmail.com>
2024-06-07 17:22:19 +0300

    Reformatted code

--- online_school/apps.py
+++ online_school/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class OnlineSchoolConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'online_school'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "online_school"
