fc78425e617dc3c377eac12e302ff9883c974add
Kris <ssspers92@gmail.com>
2024-06-07 17:22:33 +0300

    Created model Course

--- online_school/models.py
+++ online_school/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название курса")
+    preview = models.ImageField(upload_to="courses/", verbose_name="Превью курса")
+    description = models.TextField(verbose_name="Описание курса")
+    created_at = models.DateTimeField(
+        auto_now_add=True, auto_now=True, verbose_name="Дата создания"
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
