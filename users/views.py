fcfa10be00f57a53ab1eb114b6127396a8bfa599
Kris <ssspers92@gmail.com>
2024-06-11 15:59:53 +0300

    Created filter_backends. Created filterset_fields and ordering_fields

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
 from users.models import Payments
 from users.serializer import PaymentsSerializer
@@ -7,3 +9,14 @@ from users.serializer import PaymentsSerializer
 class PaymentsListAPIView(generics.ListAPIView):
     queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
+    filter_backends = (
+        DjangoFilterBackend,
+        OrderingFilter,
+    )
+    filterset_fields = (
+        "cash",
+        "transfer_to_account",
+        "courses",
+        "lessons",
+    )
+    ordering_fields = ("date_of_payment",)
