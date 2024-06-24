fcf746af75e359e2e2ead78c8aaad7a4069a9d59
Kris <ssspers92@gmail.com>
2024-06-24 21:04:16 +0300

    Created create_product for create product_id, create_price for price_id. And I created create_donation, because I use other functious together. This func created product_id, session.id, session.url

--- donations/services.py
+++ donations/services.py
@@ -7,4 +7,27 @@ def create_product(course, user):
 
     stripe.api_key = settings.STRIP_API_KEY
     product = stripe.Product.create(name=course, metadata={"owner": user})
-    return product
+    return product.id
+
+
+def create_price(price, product):
+    price = stripe.Price.create(
+        currency="rub",
+        unit_amount=price * 100,
+        product_data={"name": product},
+    )
+    return price.id
+
+
+def create_donation(course_id, user, price):
+
+    product_id = create_product(course_id, user)
+    price_id = create_price(price, product_id)
+
+    session = stripe.checkout.Session.create(
+        success_url="http://127.0.0.1:8000/course/",
+        line_items=[{"price": price_id, "quantity": 1}],
+        mode="payment",
+    )
+
+    return product_id, session.id, session.url
